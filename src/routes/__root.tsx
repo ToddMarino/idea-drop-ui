0e7146fc2ff3723c7ae60d9600cfeabf3681e80b
Todd Marino <todd.j.marino@gmail.com>
2025-12-02 14:11:17 -0600

    Not Found Page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import {
   HeadContent,
+  Link,
   Outlet,
   createRootRouteWithContext,
 } from '@tanstack/react-router';
@@ -26,6 +27,7 @@ export const Route = createRootRouteWithContext<RouterContext>()({
     ],
   }),
   component: RootLayout,
+  notFoundComponent: NotFound,
 });
 
 function RootLayout() {
@@ -49,6 +51,23 @@ function RootLayout() {
           },
         ]}
       />
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div className='flex flex-col items-center justify-center text-center py-2'>
+      <h1 className='text-4xl font-bold text-gray-800 mb-4'>404</h1>
+      <p className='text-lg text-gray-600 mb-6'>
+        Ooops. The Page you want does not exist
+      </p>
+      <Link
+        to='/'
+        className='px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition'
+      >
+        Back to the start
+      </Link>
     </div>
   );
 }
